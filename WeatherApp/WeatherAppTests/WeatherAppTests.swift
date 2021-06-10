0c2f529aa4fa9581f2e0737536a25e913a55c9ce
Surendra Karibandi <surendrakaribandi@gmail.com>
2021-06-10 11:52:17 +0530

    Update tests

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -9,52 +9,34 @@ import XCTest
 @testable import WeatherApp
 
 class WeatherAppTests: XCTestCase {
-
-
+    
+    
     var viewModel: CityViewModel? = nil
-
+    var homeViewModel: HomeViewModel? = nil
+    
     var service: HTTPManager!
     var sessionUnderTest : URLSession!
     var url: URL?
-
-
+    
+    
     override func setUp() {
         super.setUp()
-
+        
         // setting default session configuration
         sessionUnderTest = URLSession(configuration : URLSessionConfiguration.default)
-      let  weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=13.1&lon=80.3&appid=fae7190d7e6433ec3a45285ffcf55c86"
+        let  weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=13.1&lon=80.3&appid=fae7190d7e6433ec3a45285ffcf55c86"
         // setting url string directy with query parameters
         url = URL(string: weatherURL)
     }
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-
+    
+    
     //MARK: Slow failure Async test
     // API success test case
     func testAPISuccessStatus200(){
-
+        
         // status code 200
         let promise = expectation(description: "Status code : 200")
-
+        
         sessionUnderTest.dataTask(with: url!) { (data, response, error) in
             if let error = error{
                 XCTFail("Error: \(error.localizedDescription)")
@@ -68,6 +50,27 @@ class WeatherAppTests: XCTestCase {
             }
         }.resume()
         waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    //MARK: Lat/long tests
+    func testLatitude() {
+        let latitude = viewModel?.latitude
+        XCTAssertEqual(latitude, 10.56, "latitude mismatch")
+    }
+    
+    func testLongitude() {
+        let longitude = viewModel?.latitude
+        XCTAssertEqual(longitude, 9.5644, "longitude mismatch")
+    }
+    
+    func testcityDataCheck() {
+        let data = homeViewModel?.tableFeed
+        XCTAssertEqual(data?.count, 10, "city count mismatch found")
+    }
+    
+    override func tearDown() {
+        service = nil
+        super.tearDown()
     }
     
 }
